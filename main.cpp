5ec725fd170ed0fbcf3274cadd5420e1c310c353
Victor Bratko <bratko.viktor2004@gmail.com>
2023-04-18 22:29:46 +0300

    stepik_lab_2.3 version 1.1

--- main.cpp
+++ main.cpp
@@ -6,21 +6,21 @@
 
 using namespace std;
 const string fileString = "C:\\FirstCursProgramm\\C++\\stepik_lab_2.3\\text.txt";
-const vector<char> alphabet = {'(', ')', '+', '-', '*', '/'};
+const vector<string> alphabet = {"(", ")", "+", "-", "*", "/", " "};
 
 struct Stack{
-    char data = 0;
+    string data = "";
     Stack *next = nullptr;
 };
 
-void push(Stack *&head, char data){
+void push(Stack *&head, string data){
     Stack *newNode = new Stack;
     newNode->data = data;
     if(head == nullptr){
         head = newNode;
         newNode->next = nullptr;
     }else{
-        if(head->data == 0){
+        if(head->data == ""){
             head->data = data;
             return;
         }
@@ -29,22 +29,22 @@ void push(Stack *&head, char data){
     }
 }
 
-char pop(Stack *&head){
+string pop(Stack *&head){
     if(head == nullptr){
         cout << "Stack is empty";
-        return 0;
+        return "";
     }
-    char data = head->data;
+    string data = head->data;
     Stack *help = head;
     head = head->next;
     delete help;
     return data;
 }
 
-char top(Stack *&head){
+string top(Stack *&head){
     if(head == nullptr){
         cout << "Stack is empty";
-        return 0;
+        return "";
     }
     return head->data;
 }
@@ -68,8 +68,8 @@ void deleteStack(Stack *&head){
     }
 }
 
-int priority(char c){
-    switch (c) {
+int priority(string str){
+    switch (str[0]) {
         case '+':
         case '-':
             return 1;
@@ -84,20 +84,36 @@ int priority(char c){
     }
 }
 
+double action(double value1, double value2, string operation) {
+    switch (operation[0]) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            break;
+    }
+}
+
 vector<string> splitString(string str){
     vector<string> tokens;
     string str1 = "";
     for(char i : str){
-        if(find(alphabet.begin(), alphabet.end(),i) != alphabet.end()){
-            if (str1 != "") tokens.push_back(str1);
+        string help(1, i);
+        if(find(alphabet.begin(), alphabet.end(),help) != alphabet.end()){
+            if (str1 != "" && str1 != " ") tokens.push_back(str1);
             str1 = i;
-            tokens.push_back(str1);
+            if (str1 != "" && str1 != " ") tokens.push_back(str1);
             str1 = "";
         }else if(i >= '0' && i <= '9'){
             str1 += i;
         }
     }
-    if (str1 != "") tokens.push_back(str1);
+    if (str1 != "" && str1 != " ") tokens.push_back(str1);
     return tokens;
 }
 
@@ -108,23 +124,23 @@ string fromExpressionToReversePolishNotation(Stack *&stack, string str) {
         return "Wrong input!";
     }
     for (int i = 0; i < tokens.size(); i++) {
-        if (find(alphabet.begin(), alphabet.end(), tokens[i][0]) != alphabet.end()) {
-            if (tokens[i][0] == '(') {
-                push(stack, tokens[i][0]);
-            } else if (tokens[i][0] == ')') {
-                while (top(stack) != '(') {
+        if (find(alphabet.begin(), alphabet.end(), tokens[i]) != alphabet.end()) {
+            if (tokens[i] == "(") {
+                push(stack, tokens[i]);
+            } else if (tokens[i] == ")") {
+                while (top(stack) != "(") {
                     res += pop(stack);
                     res += ' ';
                 }
                 pop(stack);
             } else {
-                if (!(isEmpty(stack) || priority(top(stack)) < priority(tokens[i][0]))) {
-                    while (!isEmpty(stack) && priority(top(stack)) >= priority(tokens[i][0])) {
+                if (!(isEmpty(stack) || priority(top(stack)) < priority(tokens[i]))) {
+                    while (!isEmpty(stack) && priority(top(stack)) >= priority(tokens[i])) {
                         res += pop(stack);
                         res += ' ';
                     }
                 }
-                push(stack, tokens[i][0]);
+                push(stack, tokens[i]);
             }
         }else{
             res += tokens[i];
@@ -138,16 +154,32 @@ string fromExpressionToReversePolishNotation(Stack *&stack, string str) {
     return res;
 }
 
+string solveExpression(Stack *& stack, string str){
+    vector<string> tokens = splitString(str);
+    for(int i = 0; i < tokens.size(); i++){
+        if (find(alphabet.begin(), alphabet.end(), tokens[i]) != alphabet.end()) {
+            push(stack, to_string(action(atof(pop(stack).c_str()), atof(pop(stack).c_str()), tokens[i])));
+
+        }else{
+            push(stack, tokens[i]);
+        }
+    }
+    return pop(stack);
+}
+
 int main() {
     Stack *stack = new Stack;
-    string inputString = "";
+    string inputString, resultString, answerString;
     try {
         ifstream file(fileString);
         if (file.is_open()) {
             getline(file, inputString);
         }
         inputString.erase(remove_if(inputString.begin(), inputString.end(), ::isspace), inputString.end());
-        cout << fromExpressionToReversePolishNotation(stack,  inputString);
+        resultString = fromExpressionToReversePolishNotation(stack,  inputString);
+        deleteStack(stack);
+        answerString = solveExpression(stack, resultString);
+        cout << resultString << "\n" << answerString;
         file.close();
     }catch (int i){
         cout << "Some exception!";
